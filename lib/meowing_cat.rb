6b9070baa13c57a8c400814f033518b5fc2ce37d
Jonathan Villanueva <villanuevajonathan606@gmail.com>
2021-09-18 12:54:44 -0700

    done

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,19 @@
 ## code your solution here. 
+class Cat
+	attr_accessor :first_name, :last_name
+      
+	def name=(full_name)
+	  first_name, last_name = full_name.split
+	  @first_name = first_name
+	  @last_name = last_name
+	end
+      
+	def name
+	  "#{@first_name} #{@last_name}".strip
+	end
+
+	def meow
+		puts "meow!"
+	end 
+      
+      end
